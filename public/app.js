0117b1d8a7af8b8915bafb9a780adfbcd4d01f76
crhilditch20 <crhilditch20@gmail.com>
2017-01-27 11:55:15 +0000

    test functions to return random capital city to screen

--- public/app.js
+++ public/app.js
@@ -1,4 +1,5 @@
 var countries;
+var capitals = [];
 
 var makeRequest = function(url, callback){
   var request = new XMLHttpRequest();
@@ -11,22 +12,34 @@ var requestComplete = function(){
   if(this.status !== 200) return;
   var jsonString = this.responseText; 
   countries = JSON.parse(jsonString);
-  console.log(countries);
-  getCapital();
+  // console.log(countries);
+  populateCapitals();
+  displayCapital();
 };
 
-var getCapital = function(){
-  // var div = document.querySelector('#display-capital');
-  var capitals = [];
+var populateCapitals = function(){
   countries.forEach(function(country){
     capitals.push(country.capital);
   });
-  console.log(capitals);
+  // console.log(capitals);
+};
+
+var displayCapital = function(){
+  var div = document.querySelector('#display-capital');
+  var limit = capitals.length;
+  var index = getRandomNumber(limit);
+  div.innerHTML = capitals[index];
 }
+  function getRandomNumber(max) {
+    min = 0;
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
 
 var app = function(){
   var url = 'https://restcountries.eu/rest/v1/all';
   makeRequest(url, requestComplete);
-}
+};
 
 window.onload = app;
