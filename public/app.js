5eccc08a746d6643aa57f226e951985d2a482afe
crhilditch20 <crhilditch20@gmail.com>
2017-01-27 15:45:27 +0000

    add function to evaluate user country guess against capital and return message to screen

--- public/app.js
+++ public/app.js
@@ -1,5 +1,6 @@
 var countries;
 var capitals = [];
+var currentCapital;
 
 var makeRequest = function(url, callback){
   var request = new XMLHttpRequest();
@@ -38,7 +39,8 @@ var displayCapital = function(){
   var div = document.querySelector('#display-capital');
   var limit = capitals.length;
   var index = getRandomNumber(limit);
-  div.innerHTML = capitals[index];
+  currentCapital = capitals[index];
+  div.innerHTML = currentCapital;
 };
 
 var getRandomNumber = function(max) {
@@ -47,8 +49,25 @@ var getRandomNumber = function(max) {
     return Math.floor(Math.random() * (max - min + 1));
   };
 
+var countryGuess = function(){
+  var select = document.querySelector('#country-list');
+  var div = document.querySelector('#guess-result');
+  var userGuess = this.value;
+  var guessedCountry = getCountryObject(userGuess);
+    if(currentCapital === guessedCountry.capital){
+      div.innerHTML = "Correct!";
+    } else {
+      div.innerHTML = "Naw, guess again";
+    }
+}
 
-
+var getCountryObject = function (value) {
+  for(var country of countries){
+    if(country.name === value){
+      var countryObject = country;
+    }
+  } return countryObject;
+};
 
 var app = function(){
   var url = 'https://restcountries.eu/rest/v1/all';
@@ -56,6 +75,9 @@ var app = function(){
 
   var button = document.querySelector('#get-capital');
   button.onclick = displayCapital;
+
+  var select = document.querySelector('#country-list');
+  select.onchange = countryGuess;
 };
 
 window.onload = app;
